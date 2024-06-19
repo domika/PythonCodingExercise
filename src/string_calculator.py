626c655da1e80fc688252e0d24e5369e2f3afed1
Balazs Domonkos <Balazs_Domonkos@epam.com>
2024-06-19 10:34:35 +0200

    Minor refactor

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -1,13 +1,14 @@
 import re
 
-supported_delimiters = r'[\s,;:\-_]'
+SUPPORTED_DELIMITERS = r'[\s,;:\-_]'
+MAX_NUMBER = 100
 
 
 def calculate_sum(input_string):
     if input_string is None or input_string == "":
         return 0
     else:
-        numbers = list(map(int, re.split(supported_delimiters, input_string)))
-        filtered_numbers = [number for number in numbers if number <= 100]
+        numbers = list(map(int, re.split(SUPPORTED_DELIMITERS, input_string)))
+        filtered_numbers = [number for number in numbers if number <= MAX_NUMBER]
         total_sum = sum(filtered_numbers)
         return total_sum
