8fa8dff4f423b175e2e3b05f9d28f9dd744ce7f2
Balazs Domonkos <Balazs_Domonkos@epam.com>
2024-06-20 15:09:11 +0200

    Self review

--- src/coding_task/app.py
+++ src/coding_task/app.py
@@ -5,7 +5,7 @@ def run_app():
     user_input = input("Enter delimited numbers to add: ")
     try:
         print("Total sum: ", calculate_sum(user_input))
-    except NegativeNumbersNotSupportedException as ex:
+    except NegativeNumbersNotSupportedException:
         print("Error: Negative numbers are not supported!")
     except Exception as ex:
         print("An unexpected error occurred: ", str(ex))
